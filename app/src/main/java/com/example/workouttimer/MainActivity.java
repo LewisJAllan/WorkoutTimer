b51e53256ef1f7703c9b8a66279f7f46c838e866
Lewis Allan <bg50xx@student.sunderland.ac.uk>
2019-09-30 20:46:57 +0100

    master. UI version 1 complete.

--- app/src/main/java/com/example/workouttimer/MainActivity.java
+++ app/src/main/java/com/example/workouttimer/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.workouttimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtTimer;
+    EditText edtNumOfRounds, edtRestFrequency, edtRestTime, edtLengthOfRound;
+    ImageButton btnPlay, btnPause, btnStop;
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtTimer = (TextView) findViewById(R.id.txtTimer);
+        edtLengthOfRound = (EditText) findViewById(R.id.edtLengthOfRound);
+        edtNumOfRounds = (EditText) findViewById(R.id.edtNumOfRounds);
+        edtRestFrequency = (EditText) findViewById(R.id.edtRestFrequency);
+        edtRestTime = (EditText) findViewById(R.id.edtRestTime);
+        btnPause = (ImageButton) findViewById(R.id.btnPause);
+        btnPlay = (ImageButton) findViewById(R.id.btnPlay);
+        btnStop = (ImageButton) findViewById(R.id.btnStop);
+        btnStart = (Button) findViewById(R.id.btnStart);
     }
 }
